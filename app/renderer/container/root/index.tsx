d5f6abcec1cc4bece42ef48289e3f80fa28b069b
ViktorVan22 <zcg5368@163.com>
2022-07-08 18:00:04 +0800

    实现首页样式

--- app/renderer/container/root/index.tsx
+++ app/renderer/container/root/index.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import './index.less';
-
+import Logo from '../../../assets/YiFei_Liu.png';
 const Root = () => {
-  return <div>我是首页</div>;
+  return (
+    <div styleName="root">
+      <div styleName="container">
+        <img src={Logo} alt="" />
+        <div styleName="title">VisResumeMook</div>
+        <div styleName="tips">一个模板简历制作平台, 让你的简历更加出众 ~</div>
+        <div styleName="action">
+          {['介绍', '简历', '源码'].map((text, index) => {
+            return (
+              <div key={index} styleName="item">
+                {text}
+              </div>
+            );
+          })}
+        </div>
+        <div styleName="copyright">
+          <div styleName="footer">
+            <p styleName="copyright">
+              Copyright © 2018-{new Date().getFullYear()} All Rights Reserved. Copyright By pengdaokuan
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Root;
